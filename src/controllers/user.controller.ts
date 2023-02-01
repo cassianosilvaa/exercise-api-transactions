f8954ec31154f49913d649d6eadd68d577d9c78c
Cassiano Silva <silva11.cassiano@gmail.com>
2023-02-01 20:15:02 -0300

    firstCommit

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,14 +5,9 @@ import { ServerError } from "../errors/server.error";
 export class UserController {
     public list(req: Request, res: Response) {
         try {
-            const { age } = req.query;
             const database = new UserDatabase();
             let users = database.list();
-            if (age) {
-                users = users.filter((user) => user.age === Number(age));
-            }
             const result = users.map((user) => user.toJson());
-
             res.status(200).send({
                 ok: true,
                 message: "Users successfully listed",
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,6 +4,6 @@ import { UserController } from "../controllers/user.controller";
 export const userRoutes = () => {
     const app = Router();
 
-    app.get("/", new UserController().list);
+    app.get("/users", new UserController().list);
     return app;
 };
